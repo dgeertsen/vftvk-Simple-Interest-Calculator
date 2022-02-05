20b465b460232ca530dc4c88438ce89232d3a650
devon.geertsen <devon.geertsen>
2022-02-05 16:27:58 +0000

    test

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function compute()
 {
+    //Get principal
     var principal = document.getElementById("principal").value;
 
+    //Error if principal < 0
     if(principal == "" || principal <= 0)
     {
         alert("Enter a positive number");
@@ -9,17 +11,24 @@ function compute()
         return;
     }
 
+    //Get other variables
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    
+    //Calcualte interest
     var interest = principal * years * rate / 100;
-
+    
+    //Calcualte Date
     var dateNow = new Date();
     var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
     
+    //Display Date
     var resultDisplay = document.getElementById("result");
     resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>"  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%" + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
+
+//Display rate from slider bar
 function SliderValue()
 {
     var slider = document.getElementById("rate");
